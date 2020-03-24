0870d29b606c3a8357213769369de168a9803888
Alexiss Diaz <alexiss.diaz@live.com>
2020-03-24 12:55:09 -0500

    added public folder

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
