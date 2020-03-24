1f29604e25ffaadcdefe88cc2b75ebe108e4dd22
Alexiss Diaz <alexiss.diaz@live.com>
2020-03-24 14:02:40 -0500

    added quotations

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ require("dotenv").config();
 const app = express();
 
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, "public")));
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.engine('handlebars', exphbs({ defaultLayout: "main" }));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => {
     res.render('index');
